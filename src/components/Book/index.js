da39186d91b7153bc87328cba6738781274b646b
Val Delchev <btwonu@gmail.com>
2022-03-06 21:59:35 +0200

    Query all books from Dashboard

--- src/components/Book/index.js
+++ src/components/Book/index.js
@@ -2,39 +2,41 @@ import React from 'react';
 import './styles.scss';
 
 const Book = ({
-	id,
-	title,
-	authors,
-	url,
-	currentPage,
-	totalPages,
-	completed,
+  id,
+  title,
+  authors,
+  url,
+  currentPage,
+  totalPages,
+  completed,
 }) => {
-	const authorList = authors.map((author, i) => <li key={i}>{author}</li>);
+  const authorList = authors.map((author, i) => <li key={i}>{author}</li>);
 
-	return (
-		<li>
-			<h4>
-				<a href={url} target="_blank">
-					{title}
-				</a>
-			</h4>
+  return (
+    <li>
+      <h4>
+        <a href={url} target="_blank">
+          {title}
+        </a>
+      </h4>
 
-			<p>
-				{authorList.length && authorList.length > 1 ? 'Authors:' : 'Author:'}
-			</p>
+      <p>
+        {authorList.length && authorList.length > 1 ? 'Authors:' : 'Author:'}
+      </p>
 
-			<ul>{authorList}</ul>
+      <ul>{authorList}</ul>
 
-			<p>
-				<span>{currentPage}</span>/<span>{totalPages}</span>
-			</p>
+      {currentPage > 0 && totalPages && (
+        <p>
+          <span>{currentPage}</span>/<span>{totalPages}</span>
+        </p>
+      )}
 
-			<p>
-				Completed? : <span>{completed ? 'Yes' : 'No'}</span>
-			</p>
-		</li>
-	);
+      <p>
+        Completed? : <span>{completed ? 'Yes' : 'No'}</span>
+      </p>
+    </li>
+  );
 };
 
 export default Book;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import VideoForm from '../components/Video-form';
+import BookForm from '../components/Book-form';
+import ArticleForm from '../components/Article-form';
+import Book from '../components/Book';
 
 // Data
 import { TOPICS_DATA } from '../data-mock';
 
+// Services
+import * as db from '../services/firestore-service';
+
 const Dashboard = () => {
   const [topics, setTopics] = useState([]);
+  const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    setTopics([]);
+    db.getAll('books')
+      .then((bookArray) => {
+        const bookList = bookArray.map((book) => (
+          <Book key={book.id} {...book} />
+        ));
+
+        setBooks(bookList);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   const topicsList = topics.map((topic) => (
@@ -23,6 +39,19 @@ const Dashboard = () => {
       <h2>Dashboard</h2>
 
       <ul>{topicsList}</ul>
+
+      <VideoForm />
+
+      <hr />
+
+      <BookForm />
+
+      <hr />
+
+      <ArticleForm />
+
+      {books && <ul>{books}</ul>}
+
       <Outlet />
     </>
   );
